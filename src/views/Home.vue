c18b079b5be44e04465b69f76a0abdb754da19cd
AdrianDanlos <adrianf1team@gmail.com>
2020-06-29 12:48:16 +0200

    first commit / navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="overflow-hidden">
+    <v-row justify="end" class="ma-3">
+      <v-icon v-if="!drawer" @click.stop="drawer = !drawer" style="z-index: 10">mdi-menu</v-icon>
+      <v-icon v-if="drawer" @click.stop="drawer = !drawer" style="z-index: 10">mdi-close</v-icon>
+    </v-row>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary width="100vw">
+      <v-sheet height="100%" class="d-flex flex-column justify-center align-center purple menu">
+        <a href="#">HOME</a>
+        <a href="#">ABOUT</a>
+        <a href="#">PROJECTS</a>
+        <a href="#">CONTACT</a>
+      </v-sheet>
+    </v-navigation-drawer>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      drawer: null
+    };
   }
-}
+};
 </script>
+
+<style scoped>
+  .menu a{
+    font-size: 60px;
+    text-decoration: none;
+    color: white;
+    line-height: 80px;
+  }
+</style>
